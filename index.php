a53f2f1eb22db8a6819b083b2c57cd7c7c8ce5f6
gdm-1718-tjordewu <tjordewu@student.arteveldehs.beecho>
2017-10-05 09:43:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP - oef1</title>
+</head>
+<body>
+    <?php
+        $name = "Tjorven Dewulf";
+        echo "<h1> Welcome " . $name . "</h1>";
+    ?>
+</body>
+</html>
